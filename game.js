b61f77cb6e4ef20e8f41cdc02df8b614e510f600
Mijanur08 <mijanur08rahaman@gmail.com>
2022-06-19 17:24:47 +0530

    Simon-Game
    Simon Game Web Version

---
+++ game.js
@@ -0,0 +1,65 @@
+
+var buttonColor = ["red","blue","green","yellow"];
+
+var gamePattern = []; //initially empty;
+
+var started = false;
+
+var curr = 0;//will check the pattern
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4); //will return random integers from 0 to 3.
+    var randomChosenColour = buttonColor[randomNumber];
+
+    gamePattern.push(randomChosenColour);//adding the next button to the sequence
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);//the new button will be flashed
+    $("#level-title").text("Level "+gamePattern.length);
+    playSound(randomChosenColour);
+    curr = 0;
+}
+
+if(!started){
+    $(document).on("keypress", function(event){
+        gamePattern = [];
+        started = true;
+        nextSequence();        
+    })
+}
+
+$(".btn").click(function(){
+    let userChosenColor = this.id;
+    //console.log(userChosenColor);
+    animatePress(userChosenColor);
+    if( gamePattern[curr] == userChosenColor ){
+        playSound(userChosenColor);
+        curr++;
+    }
+
+    else{
+        $("#level-title").text("Game over! Press any Key to restart");
+        started = false;
+        playSound("wrong");
+        $("body").css({"background-color":"red"},{"opacity" : "0.8"});
+        setTimeout(function() {
+            $("body").css("background-color","#011F3F")
+        },200); //animating body color
+    }
+
+    if(curr == gamePattern.length){
+        setTimeout(function() {
+            nextSequence();
+        },500);        
+    }
+})
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
